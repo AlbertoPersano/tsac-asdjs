b5746c57ee7863981d015807bad84dc70fa52970
AlbertoPersano <alberto.persano@libero.it>
2015-11-11 11:48:16 +0100

    Hello world!!

--- Homework_01.js
+++ Homework_01.js
@@ -2,7 +2,7 @@
 array = [1, 2, 3, 5, -2];
 
 
-function ex_1_i(vettore) { 
+function ex_1_I(vettore) { 
     sum = 0;
         for (i = 0; i < vettore.length; i++) {
             if (vettore[i] < 0) {
@@ -21,7 +21,7 @@ function ex_1_i(vettore) {
 
 n = 5;
 
-function ex_2_i(numero){
+function ex_2_I(numero){
     sum = 0;
     for (i = 1; i < n; i=i+2){
         sum = sum + i;
@@ -33,7 +33,7 @@ function ex_2_i(numero){
 
 box = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function ex_3_i(vettore){
+function ex_3_I(vettore){
     sum = 0;
     for (i = 0; i < vettore.length; i++){
             sum = sum + vettore[i];
@@ -49,7 +49,7 @@ a = 6;
 b = 4;
 
 
-function ex_4_i(primo, secondo){
+function ex_4_I(primo, secondo){
     sum = 0;
     if (primo <= secondo) {
         for ( primo; primo <= secondo; primo++){
@@ -73,7 +73,7 @@ b = 3;
  
 
 
-function ex_5_i(num1, num2){
+function ex_5_I(num1, num2){
     tot = 0;
     for ( i = 0; i < b; i++){
         tot = tot + a;
@@ -90,7 +90,7 @@ b = 2;
  
 
  
-        function ex_6_i(num1, num2) { 
+        function ex_6_I(num1, num2) { 
             var intero = 0;
             var riporto = 0;
             for (num1; num1 >= num2; num1 -= num2) {
@@ -107,7 +107,7 @@ b = 2;
 num1 = 2;
 num2 = 3;
 
-function ex_7_i(a, b){
+function ex_7_I(a, b){
     if ( b == 1){
         return a;
     }
@@ -128,7 +128,7 @@ function ex_7_i(a, b){
 unvettore = [1, 2, 3, 4, 5, 6, 7, 8, 20];
  
 
-function ex_8_i(vettore){
+function ex_8_I(vettore){
     futuramatrice = [[]];
     contatore = 0;
     recordMatrix = Math.sqrt(vettore.length); // radice quadrata di un numero
@@ -153,8 +153,8 @@ qualsiasivettore = ["1", "2", "3", "4", "5"];
  
 
 
-function ex_9_i(vettore){
-    return qualsiasivettore.reverse();
+function ex_9_I(vettore){
+    return vettore.reverse();
 }
     
 
@@ -168,7 +168,7 @@ function ex_9_i(vettore){
 a = 5;
 n = 3;
 
-function ex_10_i(num1, num2){
+function ex_10_I(num1, num2){
     array = [];
     for ( i = 0; i < n; i++){
         array[i] = a
@@ -185,7 +185,7 @@ function ex_10_i(num1, num2){
 
 unvettore = [1, 2, 3, 4, 5];
 
-function ex_11_i(array){
+function ex_11_I(array){
     vettore = [];
     contatore = 0;
     for (i = 0; i < array.length; i++){
@@ -213,7 +213,7 @@ array = [1, 2, 3, -2, 5];
  
 
 
-function ex_1_r(vettore){
+function ex_1_R(vettore){
     if (vettore.length == 0 || vettore[0] < 0){
         return 0;
     }else{
@@ -229,7 +229,7 @@ numero = 5;
 i = 1;
 acc = 0;
 
-function  ex_2_r(n, i, acc){
+function  ex_2_R(n, i, acc){
     
     if ( n < i){
         return acc;
@@ -246,7 +246,7 @@ array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 con = 0;
 
 
-function  ex_3_r(a, con){
+function  ex_3_R(a, con){
     if (a.length == 0){
         return 0;
     }else if (con == a.length){
@@ -265,11 +265,11 @@ b = 4;
 i = 0;
 acc = 0;
 
-function  ex_4_r(primo, secondo, acc){
+function  ex_4_R(primo, secondo, acc){
     if ( primo <= secondo){
-        return  ex_4_r(primo + 1, secondo, acc + primo);
+        return  ex_4_R(primo + 1, secondo, acc + primo);
     }else if ( secondo <= primo){
-              return  ex_4_r(primo, secondo + 1, acc + secondo);
+              return  ex_4_R(primo, secondo + 1, acc + secondo);
      }else if (primo == secondo) {
     return acc;
     }
@@ -282,11 +282,11 @@ b = 5;
 i = 0;
 acc = 0;
 
-function  ex_5_r(a, b, i, acc){
+function  ex_5_R(a, b, i, acc){
     if (i >= b){
         return acc;
     }else{
-         return  ex_5_r(a, b, i++, acc + a);
+         return  ex_5_R(a, b, i++, acc + a);
      }
 }
                 
